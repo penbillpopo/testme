281ab9e20b9dd3bef067406eb09f91c26ec8b4bb
penbillpopo <penbillpopo@gmail.com>
2020-07-02 10:37:00 +0800

    delete stepbox

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,39 +4,19 @@
         <div id="home">
             <div class="introbox">
                 <div class="content w1200">
-                    <div class="left">
-                        <h4 class="title">
-                            Welcome TESTME!
-                        </h4>
-                        <p class="text">
-                            Do you have problem of memorize something?<br>
-                            Don't worry about that.<br>
-                            You can build your own word cards and test yourself.
-                        </p>
-                        <button class="loginbtn" type="button">
-                            <h5 class="text">start now</h5>
-                        </button>
-                    </div>
-                    <div class="right">  
-                    </div>
+                    <h4 class="title">
+                        Welcome TESTME!
+                    </h4>
+                    <p class="text">
+                        Do you have problem of memorize something?<br>
+                        Don't worry about that.<br>
+                        You can build your own word cards and test yourself.
+                    </p>
+                    <button class="loginbtn" type="button">
+                        <h5 class="text">start now</h5>
+                    </button>
                 </div>
-            </div>
-            <div class="stepbox">
-                <div class="content w1200">
-                    <div class="step">
-                        <h5 class="title">STEP 1</h5>
-                        <p class="text">Make a question</p>
-                    </div>
-                    <div class="step">
-                        <h5 class="title">STEP 2</h5>
-                        <p class="text">White the answer</p>
-                    </div>
-                    <div class="step">
-                        <h5 class="title">STEP 3</h5>
-                        <p class="text">Start test</p>
-                    </div>
-                </div>
-            </div>
+            </div>          
             <!-- <loginLB></loginLB> -->
             <!-- <registLB></registLB> -->
             <!-- <forgotLB></forgotLB> -->
