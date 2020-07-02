ec0b58dc20d53001fb123954fe050018177ee0ac
penbillpopo <penbillpopo@gmail.com>
2020-07-02 15:38:34 +0800

    add testLB

---
+++ src/components/testLB.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="testLB">
+        <div class="blackfield">
+            <div class="LBcontent">
+                <div class="headbar">
+                    <div class="textfield">
+                        <h5 class="title">
+                            Test_01
+                        </h5>
+                        <h6 class="count">
+                            1/10
+                        </h6>
+                    </div>
+                    <div class="closebtn"></div>
+                </div>
+                <div class="content">
+                    <div class="textfield">
+                        <h4 class="text">
+                            Answer
+                        </h4>
+                    </div>
+                    <div class="btnfield">
+                        <div class="ansbtnbox">
+                            <button class="correct">
+                                <h6 class="text">Correct</h6>
+                            </button>
+                            <button class="wrong">
+                                <h6 class="text">Wrong</h6>
+                            </button>
+                        </div>
+                        <!-- <button class="checkbtn" type="button">
+                            <h6 class="text">></h6>
+                        </button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" src="@/assets/scss/components/testLB.scss"></style>
--- src/pages/member.vue
+++ src/pages/member.vue
@@ -2,7 +2,8 @@
     <div>
         <navbar></navbar>
         <div id="member">
-            <router-view></router-view>            
+            <router-view></router-view>
+            <!-- <testLB></testLB>         -->
         </div>
         <foot></foot>
     </div>
@@ -11,11 +12,13 @@
 <script>
 import navbar from '@/components/navbar.vue'
 import foot from '@/components/foot.vue'
+// import testLB from '@/components/testLB.vue'
 export default {
     name: 'member',
     components:{
         navbar,
         foot,
+        // testLB
     }
 }
 </script>
